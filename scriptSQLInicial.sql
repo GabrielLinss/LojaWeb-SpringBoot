ccd4aaceead25f9e94f63a00affbcde095ac908b
GabrielLinss <glins731@gmail.com>
2018-07-12 12:27:43 -0300

    add script sql inicial

---
+++ scriptSQLInicial.sql
@@ -0,0 +1,5 @@
+﻿insert into role values('ROLE_ADMIN');
+insert into role values('ROLE_USER');
+
+insert into usuarios_roles values(1, 'ROLE_USER');
+insert into usuarios_roles values(2, 'ROLE_ADMIN');
